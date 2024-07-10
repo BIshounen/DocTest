d69092480724fcb920a0ed394de4ada42eff3e69
Bishounen <nbatyanov@gmail.com>
2024-07-10 14:15:56 +0200

    prep testing

--- customization.py
+++ customization.py
@@ -3,6 +3,19 @@ import sys
 import re
 
 
+def iterator(chapter):
+    for sub in chapter.get('sub_items', []):
+        iterator(sub.get('Chapter', None))
+
+    content = chapter.get('content','')
+
+    pattern = "\{\{customization:\s*" + customization + "\}\}([\s\S]*)\{\{\/customization\}\}"
+    replacement = r"\1"
+    new_content = re.sub(pattern, replacement, content)
+
+    chapter['content'] = new_content
+
+
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if sys.argv[1] == 'supports':      
@@ -13,13 +26,10 @@ context, book = json.load(sys.stdin)
 customization = context['config']['preprocessor']['customization']['name']
 
 for section in book['sections']:
-    content = section['Chapter']['content']
+    if 'Chapter' in section:
+        chapter = section.['Chapter']
 
-    pattern = "\{\{customization:\s*" + customization + "\}\}([\s\S]*)\{\{\/customization\}\}"
-    replacement = r"\1"
-    new_content = re.sub(pattern, replacement, content)
-
-    section['Chapter']['content'] = new_content
+        iterator(chapter)
     
 
 book['sections'][0]['Chapter']['content'] = str(book['sections'])
