383aba0cf70cbd288cd72aedfe94164a2d8fcd8d
Bishounen <nbatyanov@gmail.com>
2024-07-10 13:12:05 +0200

    testing preps

--- customization.py
+++ customization.py
@@ -5,6 +5,6 @@ import sys
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if sys.argv[1] == 'supports':
-            sys.stdout.print("test")
-            sys.stdout.print(sys.argv[2])
+            sys.stdout.write("test")
+            sys.stdout.write(sys.argv[2])
             sys.exit(1)
