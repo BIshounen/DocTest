4c6ce3092a95ccfbf5666e40bae8802a4e7549f5
Bishounen <nbatyanov@gmail.com>
2024-07-10 13:58:29 +0200

    testing

--- customization.py
+++ customization.py
@@ -12,14 +12,14 @@ context, book = json.load(sys.stdin)
 
 customization = context['config']['preprocessor']['customization']['name']
 
-for section in book['sections']:
-    content = section['Chapter']['content']
+# for section in book['sections']:
+#     content = section['Chapter']['content']
 
-    pattern = "\{\{customization:\s*" + customization + "\}\}([\s\S]*)\{\{\/customization\}\}"
-    replacement = r"\1"
-    new_content = re.sub(pattern, replacement, content)
+#     pattern = "\{\{customization:\s*" + customization + "\}\}([\s\S]*)\{\{\/customization\}\}"
+#     replacement = r"\1"
+#     new_content = re.sub(pattern, replacement, content)
 
-    section['Chapter']['content'] = new_content
+#     section['Chapter']['content'] = new_content
     
 
 book['sections'][0]['Chapter']['content'] = str(book['sections'][0])
