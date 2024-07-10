83f01683f0daa6e223ed5ac9f460531397c128fc
Bishounen <nbatyanov@gmail.com>
2024-07-10 13:08:29 +0200

    fixed print

--- customization.py
+++ customization.py
@@ -5,5 +5,5 @@ import sys
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if sys.argv[1] == 'supports':
-            print(sys.argv[2])
+            sys.stdout.write(sys.argv[2])
             sys.exit(1)
