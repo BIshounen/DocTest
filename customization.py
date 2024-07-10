22ab7c2d0ca81937a8bf5b546915a69336916c38
Bishounen <nbatyanov@gmail.com>
2024-07-10 13:13:17 +0200

    testing

--- customization.py
+++ customization.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         if sys.argv[1] == 'supports':
             sys.stdout.write("test\n")
-            sys.stdout.write(sys.argv[2])
+            sys.stdout.write(sys.argv[2] + "\n")
             sys.exit(1)
