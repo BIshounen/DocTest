33161d9d29a48ee7903cda2eab830162ddec6893
Bishounen <nbatyanov@gmail.com>
2024-07-17 13:45:35 +0200

    test children

---
+++ children.py
@@ -0,0 +1,30 @@
+import json
+import sys
+import re
+
+
+def iterator(chapter):
+    for sub in chapter.get('sub_items', []):
+        iterator(sub.get('Chapter', None))
+
+    content = chapter.get('content','')
+
+    pattern = "\{\{children\}\}"
+    replacement = "test"
+    new_content = re.sub(pattern, replacement, content)
+
+    chapter['content'] = new_content
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'supports':
+            sys.exit(0)
+
+for section in book['sections']:
+    if 'Chapter' in section:
+        chapter = section['Chapter']
+
+        iterator(chapter)
+
+print(json.dumps(book))
