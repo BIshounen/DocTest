c9127b59c59bcb9ed4ecae8265f1e0b5030734cb
Bishounen <nbatyanov@gmail.com>
2024-07-17 13:53:56 +0200

    test children

--- children.py
+++ children.py
@@ -13,7 +13,7 @@ def iterator(chapter):
     replacement = ""
 
     for sub in chapter.get('sub_items', []):
-        replacement += str(sub.get('name', 'Untitled'))
+        replacement += str(sub)
 
     new_content = re.sub(pattern, replacement, content)
 
